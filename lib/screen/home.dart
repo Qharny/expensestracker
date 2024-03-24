b14d7a299bbd36208c93556b697bb7543c815e05
Qharny <cannycc1020@gmail.com>
2024-03-24 21:53:14 +0000

    modified

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,15 +1,24 @@
-// screens/home_screen.dart
 import 'package:flutter/material.dart';
+
+import 'views/widget.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expense Tracker'),
+        title: const Text('Expense Tracker'),
       ),
       body: Center(
-        child: Text('Welcome to Expense Tracker!'),
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AddExpenseForm()),
+            );
+          },
+          child: const Text('Add New Expense'),
+        ),
       ),
     );
   }
