62b755da5713123bbee4733d11d9b1d3cc75ff01
Qharny <cannycc1020@gmail.com>
2024-03-25 03:01:15 +0000

    modified

--- lib/models/analyze.dart
+++ lib/models/analyze.dart
@@ -6,6 +6,16 @@ import 'expenses.dart';
 class AnalyticsPage extends StatelessWidget {
   final List<charts.Series<Expense, String>>
       seriesList; // List of chart series to display
+  // List<charts.Series<Expense, String>> seriesList = [
+  //   charts.Series(
+  //     id: 'expenses',
+  //     data: expenses, // List of Expense objects
+  //     domainFn: (Expense expense, _) =>
+  //         expense.category, // Function to extract category for X-axis
+  //     measureFn: (Expense expense, _) =>
+  //         expense.amount, // Function to extract amount for Y-axis
+  //   ),
+  // ];
 
   const AnalyticsPage(this.seriesList);
 
